585dc063cf206a4e0231ed5e31725a7cf6475fbf
Weede <vitali.jochim@gmx.de>
2018-05-06 21:51:27 +0200

    Projektdateien hinzufügen.

---
+++ js_electron_test1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace js_electron_test1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
